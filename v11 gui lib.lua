5a8ee2b7ec02e22f519884cb57c8f88241c8060a
SoruSM <96664850+SoruSM@users.noreply.github.com>
2022-05-22 17:43:26 +0000

    Update v11 gui lib.lua

--- v11 gui lib.lua
+++ v11 gui lib.lua
@@ -324,7 +324,7 @@ function DiscordLib:Window(text)
         if i.UserInputType == Enum.UserInputType.Keyboard and i.KeyCode == Enum.KeyCode.RightAlt then
 			if minimized == false then
 				MainFrame:TweenSize(
-					UDim2.new(0, 0, 0, 0),
+					UDim2.new(0, 681, 0, 396),
 					Enum.EasingDirection.Out,
 					Enum.EasingStyle.Quart,
 					.3,
@@ -332,7 +332,7 @@ function DiscordLib:Window(text)
 				)
 			else
 				MainFrame:TweenSize(
-					UDim2.new(0, 681, 0, 396),
+					UDim2.new(0, 0, 0, 0),
 					Enum.EasingDirection.Out,
 					Enum.EasingStyle.Quart,
 					.3,
